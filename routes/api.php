483b3c0f4a2d7d94413d79ef0facd80bd6c8852e
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-21 12:08:27 -0600

    Created route for accessing a single person

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Person;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/person', function() {
-
+Route::get('/person/{person}', function(Person $person) {
+    return $person;
 });
