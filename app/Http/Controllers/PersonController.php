ca68d79f1d319e634cdc6af96deec7c3f224785c
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-21 13:54:44 -0600

    Created Post/Store router for adding a person

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -22,4 +22,16 @@ class PersonController extends Controller
     public function index(): PersonResourceCollection {
         return new PersonResourceCollection(Person::paginate());
     }
+
+    public function store(Request $request) {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'city' => 'required'
+        ]);
+        $person = Person::create($request->all());
+        return new PersonResource($person);
+    }
 }
